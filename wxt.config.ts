4c5ff61849d24f23779e43ca06ce706b6b6a8cbd
Flmpx <flmpx18773906537@163.com>
2026-06-17 22:29:59 +0800

    refactor: 将wxt.config.ts 中的名字和介绍与 Season Switch统一

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: [],
   manifest: {
-    name: '氛围场景',
-    description: '在浏览器任意页面显示氛围场景',
+    name: 'Season Switch',
+    description: '四季变幻',
     permissions: ['storage'],
   },
 });
